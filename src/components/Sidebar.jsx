283ef9215c947da22283f5c8048597efffacd84e
Rohandede1825 <rohandede97@gmail.com>
2025-05-21 21:56:14 +0530

    spx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,6 +9,8 @@ const Sidebar = ({ onClose }) => {
   ];
   
 
+  
+
   return (
     <div className="min-h-screen w-64 bg-[#111827] text-gray-200 p-4 shadow-lg">
       <div className="flex items-center justify-between mb-8">
